7a2f2a5ef97129d54c6473e03a4d2b095fc45541
JhoanSebastianGelvesHernándezADSO <Jhoangelves1205@gmail.com>
2022-09-21 21:01:21 -0500

    4

--- main.js
+++ main.js
@@ -1,29 +1,15 @@
 addEventListener("DOMContentLoaded",()=>{
-    let n1=0,n2=0,n3=0
-    n1=Number(prompt(`ingrese el pirmer numero:`))
-    n2=Number(prompt(`ingrese el segundo numero:`))
-    n3=Number(prompt(`ingrese el tercero numero:`))
-
-        if (num1>num2 && num1>num3){
-        document.write(`El numero 1 el cual es: ${n1}es mayor`)
-        }else if (num2>num3 && num2>num1){
-        document.write(`El numero 2 el cual es: ${n2}es mayor`)
-        }else{
-        document.write(`el numero 3 el cual es: ${n3}es mayor`)
-
-        }if (num1<=num2 && num1<=num3){
-        document.write(`El numero 1 el cual es: ${n1}es menor`)
-        }else if (num2<=num3 & num2<=num1){
-        document.write(`El numero 2 el cual es: ${n2}es menor`)
-        }else{
-        document.write(`el numero 3 el cual es: ${n3}es menor`)
-
-        }if (num1==num2){
-        document.write(`El numero 1 y 2 son iguales `)
-        }else if (num2==num3){
-        document.write(`El numero 2 y 3 son iguales`)
-        }else if(num3==num1){
-        document.write(`el numero 1 y 3 son iguales`)
+    let fig=0,alt=0,bas=0,radio=0,pi=3.14
+    fig=prompt("Elija la figura a la cual desee sacarle el area (t)triangulo  (c)circulo")
+    if(fig=="t"){
+        bas=Number(prompt("Ingrese la base del triangulo: "))
+        alt=Number(prompt("Ingrese la altura del triangulo: "))
+        let pr=(bas*alt)/2
+        document.write(`El area del triangulo es de: ${pr}`)
+    }if(fig=="b"){
+        radio=Number(prompt("Ingrese el radio del circulo"))
+        let rs=(pi*radio)**2
+        document.write(`El area del circulo es de: ${rs}`)
     }
 })
 
