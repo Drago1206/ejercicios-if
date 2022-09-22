0c5633edf3ff764c243d152f08463bc5670544da
JhoanSebastianGelvesHernándezADSO <Jhoangelves1205@gmail.com>
2022-09-21 22:11:57 -0500

    13

--- main.js
+++ main.js
@@ -1,14 +1,24 @@
 addEventListener("DOMContentLoaded",()=>{
-let n1=0,n2=0
-n1=Number(prompt("Ingrese el primer digito"))
-n2=Number(prompt("Ingrese el segundo digito"))
 
-if(n1>n2){
-    let rest=(n1-n2)
-    document.write(`El resultado de la resta es: ${rest}`)
-}else{
-    alert("No se puede llevar acabo la operacion porque el segundo digito es mayor que el primero asi q valiste :3")
-}
+
+    let men=(prompt("Dijite cual es su tipo de membresia A, B, C, D:"))
+    let num1=Number(prompt("dijite el valor  del helado:"))
+    //proceso de datos
+    if (men=="A"){
+        let   res=num1*0.10
+        let   res2=num1-res
+        document.write(`su valordel helado es de:,${res2},"con el descuento de la membrecia tipo A`)
+    }else if (men=="B"){
+        let   res=num1*0.15
+        let   res2=num1-res
+        document.write(`su valordel helado es de:,${res2},"con el descuento de la membrecia tipo B`)
+    }if (men== "C"){
+        let   res=num1*0.20
+        let   res2=num1-res
+        document.write(`su valordel helado es de:,${res2},"con el descuento de la membrecia tipo C`)
+    }else if (men=="D"){
+        document.write(` su valor del helado es de : ,${num1},  sin nungun descuento`)
+    }
 })
 
 
