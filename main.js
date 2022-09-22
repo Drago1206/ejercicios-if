b95267dcb7297aff874f115455aabf273cf4c039
JhoanSebastianGelvesHernándezADSO <Jhoangelves1205@gmail.com>
2022-09-21 21:28:02 -0500

    5

--- main.js
+++ main.js
@@ -1,15 +1,12 @@
 addEventListener("DOMContentLoaded",()=>{
-    let fig=0,alt=0,bas=0,radio=0,pi=3.14
-    fig=prompt("Elija la figura a la cual desee sacarle el area (t)triangulo  (c)circulo")
-    if(fig=="t"){
-        bas=Number(prompt("Ingrese la base del triangulo: "))
-        alt=Number(prompt("Ingrese la altura del triangulo: "))
-        let pr=(bas*alt)/2
-        document.write(`El area del triangulo es de: ${pr}`)
-    }if(fig=="b"){
-        radio=Number(prompt("Ingrese el radio del circulo"))
-        let rs=(pi*radio)**2
-        document.write(`El area del circulo es de: ${rs}`)
+    let bas=0, alt=0
+    bas=Number(prompt("Ingrese la base del rectangulo para sacar su area: "))
+    alt=Number(prompt("Ingrese la altura del triangulo: "))
+    if(bas>0 && alt>0){
+        let pr=(bas*alt)
+        document.write(`El area del rectangulo es: ${pr}`)
+    }else{
+        alert("Los valores tiene que ser poositivos")
     }
 })
 
