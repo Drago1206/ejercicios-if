6070d6ef8ea383d515af9df863d11d55af83613d
JhoanSebastianGelvesHernándezADSO <Jhoangelves1205@gmail.com>
2022-09-21 20:42:50 -0500

    3

--- main.js
+++ main.js
@@ -1,13 +1,29 @@
 addEventListener("DOMContentLoaded",()=>{
     let n1=0,n2=0,n3=0
-    n1=Number(prompt("Ingrese el primer numero: "))
-    n2=Number(prompt("Ingrese el segundo numero: "))
-    n3=Number(prompt("Ingrese el tercer numero: "))
-    if(n1>n2 && n1>n3){
-        document.write(`EL primer numero que es el:${n1} es el mayor`)
-    }if(n2>n1 && n2>n3){
-        document.write(`El segundo numero el cual es: ${n2} es el mayor`)
-    }else{
-        document.write(`Èl tercer numero el cual es ${n3} es el mayor`)
+    n1=Number(prompt(`ingrese el pirmer numero:`))
+    n2=Number(prompt(`ingrese el segundo numero:`))
+    n3=Number(prompt(`ingrese el tercero numero:`))
+
+        if (num1>num2 && num1>num3){
+        document.write(`El numero 1 el cual es: ${n1}es mayor`)
+        }else if (num2>num3 && num2>num1){
+        document.write(`El numero 2 el cual es: ${n2}es mayor`)
+        }else{
+        document.write(`el numero 3 el cual es: ${n3}es mayor`)
+
+        }if (num1<=num2 && num1<=num3){
+        document.write(`El numero 1 el cual es: ${n1}es menor`)
+        }else if (num2<=num3 & num2<=num1){
+        document.write(`El numero 2 el cual es: ${n2}es menor`)
+        }else{
+        document.write(`el numero 3 el cual es: ${n3}es menor`)
+
+        }if (num1==num2){
+        document.write(`El numero 1 y 2 son iguales `)
+        }else if (num2==num3){
+        document.write(`El numero 2 y 3 son iguales`)
+        }else if(num3==num1){
+        document.write(`el numero 1 y 3 son iguales`)
     }
 })
+
