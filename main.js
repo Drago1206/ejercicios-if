1724e40718783d454203b19385598f319f9e66d4
JhoanSebastianGelvesHernándezADSO <Jhoangelves1205@gmail.com>
2022-09-21 19:45:06 -0500

    2

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 addEventListener("DOMContentLoaded",()=>{
-    let N=0
-    N=Number(prompt("Ingrese un numero positivo: "))
-    if(N<100 && N>0){
-        alert(`El numero: ${N} es menor que 100`)
+    let n1=0,n2=0,n3=0
+    n1=Number(prompt("Ingrese el primer numero: "))
+    n2=Number(prompt("Ingrese el segundo numero: "))
+    n3=Number(prompt("Ingrese el tercer numero: "))
+    if(n1>n2 && n1>n3){
+        document.write(`EL primer numero que es el:${n1} es el mayor`)
+    }if(n2>n1 && n2>n3){
+        document.write(`El segundo numero el cual es: ${n2} es el mayor`)
     }else{
-        alert("El numero tiene que ser positivo")
+        document.write(`Èl tercer numero el cual es ${n3} es el mayor`)
     }
 })
