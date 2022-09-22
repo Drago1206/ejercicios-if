1a1ffb6dd2248d6278a0f26d91553287179066b0
JhoanSebastianGelvesHernándezADSO <Jhoangelves1205@gmail.com>
2022-09-21 21:59:51 -0500

    11

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
 addEventListener("DOMContentLoaded",()=>{
-let p1=0,p2=0,p3=0,p4=0,p5=0
-p1=Number(prompt("Ingrese el valor del prducto: "))
-p2=Number(prompt("Ingrese el valor del prducto: "))
-p3=Number(prompt("Ingrese el valor del prducto: "))
-p4=Number(prompt("Ingrese el valor del prducto: "))
-p5=Number(prompt("Ingrese el valor del prducto: "))
-
-let subt=0,iva=0,total=0
-subt=(p1+p2+p3+p4+p5)
-iva=(subt*0.19)
-total=(subt+iva)
-
-alert(`El subtotal fue de:${subt} `)
-alert(`El iva fue de:${iva} `)
-alert(`El total fue de:${total} `)
+        let nombre=(prompt("ingrese su nombre:"))
+        let apellido=(prompt("ingrese su apellido:"))
+        let edad=(prompt("ingrese su edad:"))
+    
+        let not1=Number(prompt("dijite su   nota 1:"))
+        let not2=Number(prompt("dijite su   nota 2:"))
+        let not3=Number(prompt("dijite su   nota 3:"))
+        let not4=Number(prompt("dijite su   nota 4:"))
+        let not5=Number(prompt("dijite su   nota 5:"))
+    
+        let prom=(not1+not2+not3+not4+not5)/5
+        if (prom >=5){
+        alert("¡aprobado!")
+        }else{
+        alert("no aprobado")
+    }
+        document.write(`promedio de las    notas :${prom}`)
 })
 
 
